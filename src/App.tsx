37944d899703896dd7041499c9df40d8dac7cc1c
Youssef Tarek <yossef.tarekelsheikh@gmail.com>
2025-06-29 14:26:36 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import ProductCard from './Components/ProductCard'
+
+const App = () => {
+  return <>
+  <div className='border-2 border-red-50 text-s'>
+    <ProductCard/>
+    <ProductCard/>
+  </div>
+
+  </>
+}
+
+export default App
---
+++ src/Components/ProductCard.tsx
@@ -0,0 +1,14 @@
+interface IProps{
+
+}
+
+const ProductCard = ({}:IProps) => {
+  return <>
+  <div>
+    <span className="text-zinc-700">product card</span>
+  </div>
+  </>
+  
+}
+
+export default ProductCard
